c4c5eb0890a9a12ab50c3c3111c602440c2e3924
tylerjgabb <tylerjgabb@gmail.com>
2018-02-12 23:34:46 -0700

    adding teaching modules

--- js/cube.js
+++ js/cube.js
@@ -2,15 +2,17 @@ if (!document) {
     var THREE = require('three');
 }
 
+//NOTHING ABOVE THIS LINE IS ESSENTIAL ------------------------------------
+
 var renderer = new THREE.WebGLRenderer();
-//renderer.setClearColor(0xAAAAAA);
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 0.01, 1000);
 camera.position.set(10, 5, 10);
-//camera.lookAt(new THREE.Vector3(0, 0, 0));
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-var controls = new THREE.OrbitControls(camera);
+
+var controls = new THREE.OrbitControls(camera);//You don't need to know anything about this;
+controls.enablePan = false; //stops you from being able to change position of camera
 
 
 var geom = new THREE.CubeGeometry(5, 5, 5);
@@ -22,7 +24,7 @@ var cube = new THREE.Mesh(geom, mat);
 
 scene.add(cube);
 
-
+camera.lookAt(new THREE.Vector3(0, 0, 0));
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
